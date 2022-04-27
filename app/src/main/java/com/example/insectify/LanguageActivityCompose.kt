4ff090ea3de358abe4439e17612ae10de632991c
haddanreda <haddan.reda.net@gmail.com>
2022-04-27 13:00:22 +0000

    design the figma language screen & change statusbar and navbar default colors

--- app/src/main/java/com/example/insectify/LanguageActivityCompose.kt
+++ app/src/main/java/com/example/insectify/LanguageActivityCompose.kt
@@ -1,25 +1,27 @@
 package com.example.insectify
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.annotation.SuppressLint
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.insectify.ui.theme.InsectifyTheme
 import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Destination
 @Composable
 fun LanguageActivityLayout(
@@ -27,50 +29,57 @@ fun LanguageActivityLayout(
     languageLabels: List<String> = listOf("English","French","Arabic")
 ) {
     MaterialTheme {
-        ConstraintLayout (
-            modifier = Modifier
-                .fillMaxHeight()
-                .fillMaxWidth()
+        Scaffold(
+            topBar = { TopAppBar(title = {Text("Language")},backgroundColor = colorResource(R.color.blue_light))  },
+            drawerContent = { Text(text = "Drawer Menu 1") },
+            content = {
+                ConstraintLayout (
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth()
                 ) {
-            val (languagebuttons, bottombutton) = createRefs()
-            Box(
-                modifier = Modifier
-                    .constrainAs(languagebuttons) {
-                        top.linkTo(parent.top)
-                        bottom.linkTo(parent.bottom)
-                    }
-            ) {
-                // Create references for the composables to constrain
-                Column(
-                    modifier = Modifier.background(Color(0xFF7BB661))
-                ) {
-                    for (languageLabel in languageLabels) {
-                        Row (
-                            modifier = Modifier
-                                .background(Color(0xFF7FF661))
-                                .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.Center
-                        ) {
-                            LanguageButton(languageLabel = languageLabel)
+                    val (languagebuttons, bottombutton) = createRefs()
+                    Box(
+                        modifier = Modifier
+                            .constrainAs(languagebuttons) {
+                                top.linkTo(parent.top)
+                                bottom.linkTo(parent.bottom)
+                            }
+                    ) {
+                        // Create references for the composables to constrain
+                        Column {
+                            for (languageLabel in languageLabels) {
+                                Row (
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(
+                                            top = 20.dp,
+                                            bottom = 20.dp
+                                        ),
+                                    horizontalArrangement = Arrangement.Center
+                                ) {
+                                    LanguageButton(languageLabel = languageLabel)
+                                }
+                            }
                         }
                     }
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .constrainAs(bottombutton) {
+                                bottom.linkTo(parent.bottom)
+                            },
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        BottomButton(
+                            navigator,
+                            buttonText = "CONTINUE",
+                            Screen.PredictScreen.route)
+                    }
                 }
-            }
-            Row (
-                modifier = Modifier
-                    .background(Color(0xFF7FF661))
-                    .fillMaxWidth()
-                    .constrainAs(bottombutton) {
-                        bottom.linkTo(parent.bottom)
-                    },
-                horizontalArrangement = Arrangement.Center
-            ) {
-                BottomButton(
-                    navigator,
-                    buttonText = "CONTINUE",
-                    Screen.PredictScreen.route)
-            }
-        }
+                      }
+        )
+
     }
 }
 
@@ -79,17 +88,9 @@ fun LanguageButton(languageLabel: String) {
     MaterialTheme {
         var isClicked by remember { mutableStateOf(false) }
         val surfaceColor: Color by animateColorAsState(
-            if (isClicked) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+            if (isClicked) colorResource(R.color.blue_light) else colorResource(R.color.grey) ,
         )
         // Material Components like Button, Card, Switch, etc.
-        Box(
-            Modifier.padding(
-                start = 30.dp,
-                end = 30.dp,
-                top = 20.dp,
-                bottom = 20.dp
-            )
-        ) {
             Button(
                 shape = RoundedCornerShape(15.dp),
                 colors = ButtonDefaults.buttonColors(
@@ -100,20 +101,20 @@ fun LanguageButton(languageLabel: String) {
                 },
                 modifier = Modifier
                     .width(200.dp)
-                    .height(60.dp)
+                    .height(75.dp)
 
             ) {
                 // Inner content including an icon and a text label
                 Spacer(Modifier.size(ButtonDefaults.IconSpacing))
                 Text(text = "$languageLabel",fontSize = 30.sp)
             }
-        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview2() {
+fun LanguageScreenPreview() {
     InsectifyTheme {
+        LanguageActivityLayout(navigator = rememberNavController())
     }
 }
