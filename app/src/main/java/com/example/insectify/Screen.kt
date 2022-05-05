77e383493315c5128dd58405a32db8a1fe616787
haddanreda <haddan.reda.net@gmail.com>
2022-05-05 11:17:32 +0000

    remove languagescreen from navgraph & screen class

--- app/src/main/java/com/example/insectify/Screen.kt
+++ app/src/main/java/com/example/insectify/Screen.kt
@@ -2,7 +2,6 @@ package com.example.insectify
 
 sealed class Screen (val route:String) {
     object WelcomeScreen: Screen(route = "welcome_screen")
-    object LanguageScreen: Screen(route = "language_screen")
     object PredictScreen: Screen(route = "predict_screen")
     object DetailsScreen: Screen(route = "details_screen")
 }
